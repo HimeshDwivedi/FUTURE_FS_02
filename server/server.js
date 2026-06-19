e010cc1e8f693163d99571ffcc0d41cb6bd96040
Himesh Kumar Dwivedi <hkdwivedi1000@gmail.com>
2026-06-19 13:39:43 +0530

    Fixed auth request body parsing

--- server/server.js
+++ server/server.js
@@ -9,8 +9,9 @@ const leadRoutes=require("./routes/leadRoutes")
 const app=express()
 
 app.use(cors())
-app.use("/api/auth",authRoutes)
 app.use(express.json())
+app.use("/api/auth",authRoutes)
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
